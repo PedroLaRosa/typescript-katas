478e7ba37690639137f53191afca14b72d2c5105
Pedro La Rosa D. <pedrolarosad@gmail.com>
2026-03-01 00:58:31 +0100

    tests(markdown): handle duplicated links

--- src/markdown-kata/markdown-transformer.test.ts
+++ src/markdown-kata/markdown-transformer.test.ts
@@ -4,13 +4,17 @@ import { MarkdownTransformer } from "./markdown-transformer.js";
 describe("The Markdown Transformer", () => {
   it("should not transform a given markdown text that does no contain any links", () => {
     const markdownText = "This is a text without links";
+
     const transformedText = new MarkdownTransformer(markdownText).transform();
+
     expect(transformedText).toBe(markdownText);
   });
 
   it("should transform a given markdown text that contains one link", () => {
     const markdownText = "[Visible text link](url)";
+
     const transformedText = new MarkdownTransformer(markdownText).transform();
+
     expect(transformedText).toBe(
       "Visible text link [^anchor]\n\n[^anchor1]: url",
     );
@@ -19,9 +23,22 @@ describe("The Markdown Transformer", () => {
   it("should transform a given markdown text that contains two links", () => {
     const markdownText =
       "blah blah blah... [Visible text link](url) blah blah [this is a link](other-url)";
+
     const transformedText = new MarkdownTransformer(markdownText).transform();
+
     expect(transformedText).toBe(
       "blah blah blah... Visible text link [^anchor] blah blah this is a link [^anchor]\n\n[^anchor1]: url\n\n[^anchor2]: other-url",
+    );
+  });
+
+  it("should not keep duplicate links", () => {
+    const markdownText =
+      "[Visible text link](url) Something else [Visible text link](url)";
+
+    const transformedText = new MarkdownTransformer(markdownText).transform();
+
+    expect(transformedText).toBe(
+      "Visible text link [^anchor] Something else Visible text link [^anchor]\n\n[^anchor1]: url",
     );
   });
 });
