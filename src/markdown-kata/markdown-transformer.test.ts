0d5aabb56843e5a25f1b7cc9712a55f7d1f5dd8e
Pedro La Rosa D. <pedrolarosad@gmail.com>
2026-02-28 02:51:53 +0100

    kata(markdown): draft implementation with unstable api

---
+++ src/markdown-kata/markdown-transformer.test.ts
@@ -0,0 +1,182 @@
+import { describe, expect, it } from "vitest";
+
+class MarkdownTransformer {
+  constructor(readonly markdownText: string) {}
+
+  transform() {
+    return this.markdownText;
+  }
+
+  findAllLinks() {
+    const linkRegex = /\[([^\]]+)\]\(([^\)]+)\)/g;
+    const matchesAsList = Array.from(this.markdownText.matchAll(linkRegex));
+    const links = matchesAsList
+      .map((match) => {
+        const [_, text, url] = match;
+        if (!text || !url) return;
+        return new MarkdownLink(text, url);
+      })
+      .filter((link) => link !== undefined);
+    return this.uniqueLinks(links);
+  }
+
+  uniqueLinks(links: MarkdownLink[]) {
+    return links.filter((link, index) => {
+      return links.findIndex((l) => l.toEqual(link)) === index;
+    });
+  }
+
+  generateLinkRecord(links: MarkdownLink[]) {
+    return links.reduce<Record<string, MarkdownLink>>(
+      (dictionary, link, index) => {
+        return {
+          ...dictionary,
+          [`[^anchor${index + 1}]`]: link,
+        };
+      },
+      {},
+    );
+  }
+
+  replaceLinksByAnchor(linkRecord: Record<string, MarkdownLink>) {
+    return Object.keys(linkRecord).reduce((previousText, key, index) => {
+      return previousText.replaceAll(
+        linkRecord[key]!.toAnchorFormat(),
+        `${linkRecord[key]!.text} [^anchor]`,
+      );
+    }, this.markdownText);
+  }
+
+  createFootNotes(linkRecord: Record<string, MarkdownLink>) {
+    return Object.keys(linkRecord).map(
+      (footnoteKey) => `${footnoteKey}: ${linkRecord[footnoteKey]!.url}`,
+    );
+  }
+
+  appendFootnotesToMarkDown(transformedMarkdown: string, footNotes: string[]) {
+    return [transformedMarkdown, ...footNotes].join("\n\n");
+  }
+}
+
+class MarkdownLink {
+  constructor(
+    readonly text: string,
+    readonly url: string,
+  ) {}
+
+  toEqual(markdownLink: MarkdownLink) {
+    return this.text === markdownLink.text && this.url === markdownLink.url;
+  }
+
+  toAnchorFormat() {
+    return `[${this.text}](${this.url})`;
+  }
+}
+
+describe("The Markdown Transformer", () => {
+  it("should not transform a given markdown text that does no contain any links", () => {
+    const markdownText = "This is a text without links";
+    const transformedText = new MarkdownTransformer(markdownText).transform();
+    expect(transformedText).toBe(markdownText);
+  });
+
+  it("should transform a given markdown text that contains one link", () => {
+    const markdownText = "[Visible text link](url)";
+    const transformedText = new MarkdownTransformer(markdownText).transform();
+    expect(transformedText).toBe(
+      "Visible text link [^anchor] \n\n [^anchor1]: url",
+    );
+  });
+});
+
+describe("Find all links", () => {
+  it("should not find links in a given markdown text that does no contain any links", () => {
+    const markdownText = "";
+    const transformer = new MarkdownTransformer(markdownText);
+
+    const allLinks = transformer.findAllLinks();
+
+    expect(allLinks).toEqual([]);
+  });
+
+  it("should all find links in a given markdown text", () => {
+    const markdownText =
+      "[Visible text link](url)[other Visible text link](other-url)";
+    const transformer = new MarkdownTransformer(markdownText);
+
+    const allLinks = transformer.findAllLinks();
+
+    expect(allLinks).toEqual([
+      new MarkdownLink("Visible text link", "url"),
+      new MarkdownLink("other Visible text link", "other-url"),
+    ]);
+  });
+
+  it("should only non-duplicated links", () => {
+    const markdownText =
+      "[Visible text link](url)[other Visible text link](other-url)[Visible text link](url)";
+    const transformer = new MarkdownTransformer(markdownText);
+
+    const allLinks = transformer.findAllLinks();
+
+    expect(allLinks).toEqual([
+      new MarkdownLink("Visible text link", "url"),
+      new MarkdownLink("other Visible text link", "other-url"),
+    ]);
+  });
+});
+
+describe("Generate links record", () => {
+  it("Should generate a record with all links", () => {
+    const markdownText =
+      "[Visible text link](url)[other Visible text link](other-url)[Visible text link](url)";
+    const transformer = new MarkdownTransformer(markdownText);
+
+    const link1 = new MarkdownLink("Visible text link", "url");
+    const link2 = new MarkdownLink("other Visible text link", "other-url");
+
+    const allLinks = transformer.generateLinkRecord([link1, link2]);
+
+    expect(allLinks).toEqual({
+      ["[^anchor1]"]: new MarkdownLink("Visible text link", "url"),
+      ["[^anchor2]"]: new MarkdownLink("other Visible text link", "other-url"),
+    });
+  });
+
+  it("should replace text links by anchors", () => {
+    const markdownText = "[Visible text link](url)";
+    const transformer = new MarkdownTransformer(markdownText);
+
+    const links = transformer.findAllLinks();
+    const linkRecord = transformer.generateLinkRecord(links);
+    const transformedText = transformer.replaceLinksByAnchor(linkRecord);
+
+    expect(transformedText).toBe("Visible text link [^anchor]");
+  });
+
+  it("creates footnotes", () => {
+    const markdownText = "[Visible text link](url)";
+    const transformer = new MarkdownTransformer(markdownText);
+    const links = transformer.findAllLinks();
+    const linkRecord = transformer.generateLinkRecord(links);
+    const footNotes = transformer.createFootNotes(linkRecord);
+
+    expect(footNotes).toEqual(["[^anchor1]: url"]);
+  });
+
+  it("appends footnotes to markdown", () => {
+    const markdownText = "[Visible text link](url)";
+    const transformer = new MarkdownTransformer(markdownText);
+    const links = transformer.findAllLinks();
+    const linkRecord = transformer.generateLinkRecord(links);
+    const transformedMarkdown = transformer.replaceLinksByAnchor(linkRecord);
+    const footNotes = transformer.createFootNotes(linkRecord);
+
+    const transformedMarkdownWithFootNotes =
+      transformer.appendFootnotesToMarkDown(transformedMarkdown, footNotes);
+
+    expect(transformedMarkdownWithFootNotes).toBe(
+      "Visible text link [^anchor]\n\n[^anchor1]: url",
+    );
+  });
+});
